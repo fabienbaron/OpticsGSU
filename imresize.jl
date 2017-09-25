1b20be15441b6372cdb9066cc74dbb1c7e183f0d
Fabien Baron <baron@chara.gsu.edu>
2017-09-25 12:22:26 -0400

    Ready

--- imresize.jl
+++ imresize.jl
@@ -1,4 +1,11 @@
+if Pkg.installed("Images.jl") == nothing
+           println("Please install the Images package using Pkg.add(\"Images\")\n");
+end
+
 using Images
+using FITSIO
+using PyPlot
+
 function imscale(image, scale)
 newsize = (Int(round(scale*size(image,1))), Int(round(scale*size(image,2))))
 return Images.imresize(image, newsize)
@@ -9,8 +16,6 @@ function imresize(image, newsize)
 end
 
 function test_resize()
-using FITSIO
-using PyPlot
 image=read(FITS("saturn.fits")[1])
 #image = image / sum(image)
 imshow(image)
