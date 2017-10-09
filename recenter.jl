cca845b9580ed7fcb8542659e29a2add1c701a63
Fabien Baron <baron@chara.gsu.edu>
2017-10-09 13:04:39 -0400

    Recentering

--- recenter.jl
+++ recenter.jl
@@ -1,9 +1,9 @@
 function cog(x) #note: this takes a 2D array
 xvals=[i for i=1:size(x,1)]
-return [sum(xvals'*x) sum(x*xvals)]/sum(x)
+return vec([sum(xvals'*x) sum(x*xvals)]/sum(x))
 end
 
 function recenter(x) #note: this takes a 2D array
-Δ=cog(x)-( (size(x,1)+1)/2, (size(x,2)+1)/2)
+Δ=[(size(x,1)+1)/2, (size(x,2)+1)/2]-cog(x)
 return circshift(x,round.(Δ))
 end
