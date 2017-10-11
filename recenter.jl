c9bd1df33c09f70beb71e7fd18b99b9df75500e2
Fabien Baron <baron@chara.gsu.edu>
2017-10-11 11:27:19 -0400

    Change to recenter

--- recenter.jl
+++ recenter.jl
@@ -3,7 +3,12 @@ xvals=[i for i=1:size(x,1)]
 return vec([sum(xvals'*x) sum(x*xvals)]/sum(x))
 end
 
-function recenter(x) #note: this takes a 2D array
-Δ=[(size(x,1)+1)/2, (size(x,2)+1)/2]-cog(x)
+function posmax(x) #note: this takes a 2D array
+pos = ind2sub(size(x),indmax(x))
+return [pos[1],pos[2]]
+end
+
+function recenter(x, newcenter) #note: this takes a 2D array
+Δ=[(size(x,1)+1)/2, (size(x,2)+1)/2]-newcenter
 return circshift(x,round.(Δ))
 end
