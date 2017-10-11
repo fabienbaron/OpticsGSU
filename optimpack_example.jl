4419ab42759af20109250d10e9a668a2126a2efe
Fabien Baron <baron@chara.gsu.edu>
2017-10-11 12:32:13 -0400

    Added optimpack example

---
+++ optimpack_example.jl
@@ -0,0 +1,13 @@
+using OptimPack
+
+function chi2_fg(x, g, data) # criterion function plus its gradient w/r x
+  f = sum((x-data).^2)
+  g[:] = 2*(x-data)
+  return f
+end
+
+data = rand(100).^2;
+x_start = zeros(100);
+
+crit = (x,g)->chi2_fg(x, g, data);
+x_sol = OptimPack.vmlmb(crit, x_start, verb=true, lower=0, maxiter=80);
