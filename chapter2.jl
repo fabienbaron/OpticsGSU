7f97f3b9297ded2fe94e1850884dcdebca8dee98
Fabien Baron <baron@chara.gsu.edu>
2017-10-18 14:52:15 -0400

    Update

--- chapter2.jl
+++ chapter2.jl
@@ -1,14 +1,13 @@
 using OptimPack
 include("optics.jl")
-obj = read(FITS("jupiter.fits")[1])*1.;
+x_true = read(FITS("jupiter.fits")[1])*1.;
 # Generate data
-x_true = zeros(256,256); x_true[190:200,190:200]=1.0;
-npix = size(obj,1);
-#psf = gaussian2d(npix,npix,8)
-psf= zeros(256,256); psf[125:131,128]=1.0; psf[128,125:131]=1.0;
+npix = size(x_true,1);
+psf = gaussian2d(npix,npix,3);
+#psf= zeros(npix,npix); psf[125:131,128]=1.0; psf[128,125:131]=1.0;
 x_conv = convolve(psf,x_true);
 σ = 1e-5#maximum(x_true)/10.;
-x_noisy = x_conv + σ.*randn(size(obj));
+x_noisy = x_conv + σ.*randn(size(x_true));
 
 # Function to minimize + gradient w/r object pixels
 function epsilon(model, g, data, σ, psf)
@@ -18,7 +17,7 @@ function epsilon(model, g, data, σ, psf)
     return f;
 end
 
-initial_guess = copy(x_noisy);
+initial_guess = rand(size(x_noisy));
 crit = (x,g)->epsilon(x, g, x_noisy, σ, psf);
 x_sol = OptimPack.vmlmb(crit, initial_guess, verb=true, lower=0, maxiter=500);
 imview4(x_true, x_noisy, x_sol, convolve(x_sol,psf))
