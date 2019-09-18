92b4741008dccd8f570bee0959500e53c05cdcdb
Fabien Baron <baron@phy-astr.gsu.edu>
2019-09-18 12:09:23 -0400

    Updated chapter1c.jl

--- demos/chapter1c.jl
+++ demos/chapter1c.jl
@@ -7,10 +7,8 @@ phase_orig=readfits("./data/Dr03_phases.fits.fz");
 npix = size(phase_orig,1)
 
 # Number of phase screens to use (max=500)
-jlim=500
-
-#Initialize array
-phase=zeros(size(phase_orig));
+var_screen = var(phase_orig);
+dr0 = (var_screen/1.0299)^(3/5);
 
 # Compute second and third Zernike maps (for tip and tilt): center is at [npix/2+1,npix/2+1]
 zern1 = zernike(1,npix=npix,diameter=npix,centered=true);
@@ -23,16 +21,19 @@ phase = phase_orig .* aperture;
 
 indx= findall(repeat(aperture,outer=[1,1,500]).>0.5);
 # Compute d/r0 over subsection all phase pixels
-std_dev = std(phase[indx]);
-dr0 = (std_dev^2/1.03)^(3/5);
+var_pupil = var(phase[indx]);
+newdr0 = (var_pupil/1.0299)^(3/5);
 
 #scale phase to be consistent with required d/r0
-#phase = phase  * (newdr0/dr0)^(5/6);
-#std_dev1 = std(phase[50:200,50:200,1:jlim]);
-#dr0=(std_dev1^2/1.03)^(3/5);
+phase = phase  * (dr0/newdr0)^(5/6);
+var_pupil = var(phase[indx]);
+dr0 = (var_pupil/1.0299)^(3/5);
+
 
 # Perform Zernike decomposition for tip and tilt and remove from phases
 # N.B. dot product gives pi x coeff, therefore need to normalize coeff by pi
+jlim=500
+
 for j = 1:jlim
   a1 = 0. #sum(zern1.*phase[:,:,j])/3.1415926;
   a2 = sum(zern2.*phase[:,:,j])/3.1415926;
@@ -40,16 +41,13 @@ for j = 1:jlim
   phase[:,:,j] =  phase[:,:,j] - (a1*zern1 + a2*zern2 + a3*zern3);
 end
 
-#Output fits image of corrected phases
-writefits(phase, "corrected.fits");
-
 #Output D/r0 value in section
-println("D/r0 over pupil: ",dr0);
+println("D/r0 over pupil: ", dr0);
 #Output standard deviation for sub-section of phase screen in pupil before t/t correction
-println("std. dev. of phase: ",std_dev,"  [radians]");
+println("std. dev. of phase: ",sqrt(var_pupil),"  [radians]");
 #Output standard deviation for sub-section of phase screen in pupil after t/t correction
-std_dev2 = std(phase[indx]);
-println("std. dev of phase after tip/tilt subtraction: ",std_dev2,"  [radians]");
+var_pupil_tt = var(phase[indx]);
+println("std. dev of phase after tip/tilt subtraction: ",sqrt(var_pupil_tt),"  [radians]");
 
 # Output coefficient for tip/tilt corrected phases
- println("Coeff. for relation between variance and D/r0 with tip/tilt removal: ",std_dev2^2/dr0^(5/3.));
+ println("Coeff. for relation between variance and D/r0 with tip/tilt removal: ",var_pupil_tt/dr0^(5/3.));
